e9c6f21d288632be36f430fd9a5bfa3299c28601
k21016679 <adam.moujar_bakhti@kcl.ac.uk>
2022-09-28 13:16:10 +0100

    Find median given an array of integers

--- median.py
+++ median.py
@@ -9,4 +9,16 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
+
+numbers.sort()
+
+if(len(numbers) % 2 == 1):
+    index = (len(numbers) + 1) / 2
+    result = numbers[int(index-1)]
+
+else:
+    index = len(numbers) / 2
+    result = (numbers[int(index)] + numbers[int(index) - 1]) / 2
+
+print(result)
 print(numbers)
